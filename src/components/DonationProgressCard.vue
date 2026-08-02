94f41ab232ff52f3b5f18a92694b6a9e659b9694
Banpot Sangsue <banpot@live.com>
2026-08-03 02:06:50 +0700

    version 0.6.1-dev.1  - added total vote tokens

--- src/components/DonationProgressCard.vue
+++ src/components/DonationProgressCard.vue
@@ -64,8 +64,10 @@
         </div>
         <div class="text-sm sm:text-base font-bold text-[#457b9d] font-mono mt-1">
           / ฿{{ formatMoney(state.config.targetGoal) }}
+        </div>
+        <div class="text-sm sm:text-base font-bold text-[#457b9d] font-mono mt-1">
           <span v-if="activeBonusTierMultiplier > 0" class="ml-2 inline-block px-2 py-0.5 bg-emerald-100 text-emerald-800 border border-emerald-600 rounded text-xs">
-            ผู้สนับสนุนทบ {{ formatMoney(activeBonusTierMultiplier) }} Tokens
+            ผู้สนับสนุนทบ {{ formatMoney(activeBonusTierMultiplier) }} Tokens • ซื้อ {{ formatMoney(totalAmount/68) }} Tokens • รวม Vote {{ formatMoney(activeBonusTierMultiplier + totalAmount/68) }} Tokens
           </span>
         </div>
       </div>
